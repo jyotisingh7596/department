be8146971d4767ec2cdc35410db865b8d412306a
jyotisingh7596 <75801620+jyotisingh7596@users.noreply.github.com>
2021-01-31 20:59:21 +0530

    created department service

---
+++ src/main/java/com/jyotisingh/department/service/DepartmentService.java
@@ -0,0 +1,13 @@
+package com.jyotisingh.department.service;
+
+import com.jyotisingh.department.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+
+    @Autowired
+    private DepartmentRepository departmentRepository;
+    
+}
